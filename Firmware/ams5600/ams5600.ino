7c1121d3fbccbf20bcef35b1a1fb6611fe42075d
Christian Prather <christian@luci.com>
2023-05-31 17:03:04 -0600

    Inital Commit

---
+++ Firmware/ams5600/ams5600.ino
@@ -0,0 +1,103 @@
+#include <Wire.h>
+#include <AS5600.h>
+AMS_5600 encoder;
+
+#define TCAADDR 0x70
+
+#define LEFT_WHEEL 1
+#define RIGHT_WHEEL 4
+#define FL_CASTOR 0
+#define BL_CASTOR 2
+#define FR_CASTOR 5
+#define BR_CASTOR 3
+
+// Buffer to be sent over serial that luci sensors receives and parses
+// Luci sensors is looking for the tag "+AR NCDR="
+byte buf[25] = {'+', 'A', 'R', ' ', 'N', 'C', 'D', 'R', '=', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+void tcaselect(uint8_t i)
+{
+    if (i > 7)
+    {
+        return;
+    }
+    Wire.beginTransmission(TCAADDR);
+    Wire.write(1 << i);
+    Wire.endTransmission();
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    Wire.begin();
+    tcaselect(1);
+
+    if (!encoder.detectMagnet())
+    {
+        while (true)
+        {
+            if (encoder.detectMagnet())
+            {
+                encoder.getMagnitude();
+                break;
+            }
+            delay(1000);
+        }
+    }
+}
+
+float convertRawAngleToDegrees(unsigned int newAngle)
+{
+    // 360 degrees / 4096 unique readings = 0.087890625
+    float ang = newAngle * 0.088;
+
+    return ang;
+}
+
+void loop()
+{
+    // Get the data from all 6 encoders
+    // Switch the decimal to the right by 2 for both wheel angles in order to send over
+    // a long that includes wheel angle precision to the hundredth of a degree
+    tcaselect(LEFT_WHEEL);
+    long leftWheelAng = long(convertRawAngleToDegrees(encoder.getRawAngle()) * 100);
+    tcaselect(RIGHT_WHEEL);
+    long rightWheelAng = long(convertRawAngleToDegrees(encoder.getRawAngle()) * 100);
+    tcaselect(FL_CASTOR);
+    int front_left_castor = int(convertRawAngleToDegrees(encoder.getRawAngle()));
+    tcaselect(BL_CASTOR);
+    int back_left_castor = int(convertRawAngleToDegrees(encoder.getRawAngle()));
+    tcaselect(FR_CASTOR);
+    int front_right_castor = int(convertRawAngleToDegrees(encoder.getRawAngle()));
+    tcaselect(BR_CASTOR);
+    int back_right_castor = int(convertRawAngleToDegrees(encoder.getRawAngle()));
+
+    // Prepare the buffer to be sent over serial to luci sensors
+    // [ 1 - 9     | 10 11 12 13 | 14 15 16 17 | 18 19 | 20 21 | 22 23 | 24 25 ]
+    // [ +AR NCDR= | LEFT W      | Right W     | FL    | BL    | FR    | BR    ]
+    buf[9] = leftWheelAng;
+    buf[10] = leftWheelAng >> 8;
+    buf[11] = leftWheelAng >> 16;
+    buf[12] = leftWheelAng >> 24;
+
+    buf[13] = rightWheelAng;
+    buf[14] = rightWheelAng >> 8;
+    buf[15] = rightWheelAng >> 16;
+    buf[16] = rightWheelAng >> 24;
+
+    buf[17] = front_left_castor;
+    buf[18] = front_left_castor >> 8;
+
+    buf[19] = back_left_castor;
+    buf[20] = back_left_castor >> 8;
+
+    buf[21] = front_right_castor;
+    buf[22] = front_right_castor >> 8;
+
+    buf[23] = back_right_castor;
+    buf[24] = back_right_castor >> 8;
+
+    int bytesWritten = Serial.write(buf, sizeof(buf));
+
+    delay(50);
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# luci-sdk-encoders
+# luci-sdk-encoders #
 Design files and documentation for power wheelchair encoders compatible with the LUCI sensor system
+
+### What is this repository for? ###
+
+* Quick summary
+* Version
+* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
